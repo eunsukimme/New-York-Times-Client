a521290f5265c349c586aebaa1b32cc23a328904
eunsukimme <eunsu.dev@gmail.com>
2020-05-10 21:43:24 +0900

    fix: Fix cors issue by adding default 'api-key' param

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -3,7 +3,21 @@ import axios from "axios";
 const client = axios.create();
 
 client.defaults.baseURL = process.env.REACT_APP_API_URL;
-client.defaults.headers.common["apiKey"] = process.env.REACT_APP_API_KEY;
-// client.defaults.withCredentials = true;
+/** add default params */
+client.interceptors.request.use((config) => {
+  config.params = config.params || {};
+  config.params["api-key"] = process.env.REACT_APP_API_KEY;
+  return config;
+});
+/** add interceptor for reaponse */
+// Add a response interceptor
+client.interceptors.response.use(
+  (res) => res.data.response,
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 export default client;
